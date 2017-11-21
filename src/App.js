7eb1665fdbc7354c56b3158e3f4f7e378eee54f5
nikolalsvk <nikolaseap@gmail.com>
2017-11-21 22:05:15 +0000

    Add Bootstrap for design

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { Jumbotron, Grid } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Jumbotron>
+        <Grid>
+          <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Welcome to acquire-it</h1>
+          </header>
+          <p>Handling insurance payments for wandering travellers</p>
+        </Grid>
+      </Jumbotron>
     );
   }
 }
